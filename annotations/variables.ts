149ba1a975829d183fa856ffcb2bfb298f9a7d51
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2022-01-16 12:19:43 +0500

    array object and class type innotation

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -9,3 +9,18 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue'];
+
+let myNumbers: number[] = [1, 3, 5];
+
+let bool: boolean[] = [true, false];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+  x: 20,
+  y: 10,
+};
