f1b4b2e47e4dbfdca1cfd49d8200c6d18485ecdd
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2022-01-16 12:28:35 +0500

    type annotation function

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -8,19 +8,28 @@ let nothingMuch: null = null;
 
 let nothing: undefined = undefined;
 
+// pre defined object
 let now: Date = new Date();
 
+// array
 let colors: string[] = ['red', 'green', 'blue'];
 
 let myNumbers: number[] = [1, 3, 5];
 
 let bool: boolean[] = [true, false];
 
+// class
 class Car {}
 
 let car: Car = new Car();
 
+// object literals
 let point: { x: number; y: number } = {
   x: 20,
   y: 10,
+};
+
+// function
+let logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
 };
