66f5a8adb5ccefa8cb2017855bfeee4f051a5f4b
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2022-01-30 15:32:53 +0500

    object types

--- tuples.ts
+++ tuples.ts
@@ -11,3 +11,10 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ["brown", true, 40];
 const sprit: Drink = ["clear", true, 40];
 const tea: Drink = ["brown", false, 0];
+
+const carSpec: [number, number] = [300, 4521];
+
+const carStatic = {
+  power: 300,
+  weight: 4521,
+};
