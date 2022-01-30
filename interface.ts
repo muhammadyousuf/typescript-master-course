7cd10ace80c341a7645ae936fa1beffcc8e3a315
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2022-01-30 21:22:17 +0500

    interface completed

--- interface.ts
+++ interface.ts
@@ -11,8 +11,18 @@ const oldCivic = {
   },
 };
 
+const drinks = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of suger`;
+  },
+};
+
 const printSummary = (item: Reportable) => {
   console.log(item.summary());
 };
 
 printSummary(oldCivic);
+printSummary(drinks);
